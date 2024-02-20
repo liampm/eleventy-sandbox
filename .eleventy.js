2261b3a700a98596e4dfbe9ba9004ba813bc1939
Liam Morriss <liam717@gmail.com>
2024-02-20 15:24:47 +0000

    Add pages to test regeneration.

---
+++ .eleventy.js
@@ -0,0 +1,34 @@
+const { EleventyServerlessBundlerPlugin } = require('@11ty/eleventy')
+const eleventyNavigationPlugin = require('@11ty/eleventy-navigation')
+
+module.exports = eleventyConfig => {
+
+  eleventyConfig.addPlugin(EleventyServerlessBundlerPlugin, {
+    name: 'cache-page', // The serverless function name for the permalink object
+    functionsDir: './cloud/functions/',
+  })
+
+  eleventyConfig.addPlugin(EleventyServerlessBundlerPlugin, {
+    name: 'ssr', // The serverless function name for the permalink object
+    functionsDir: './cloud/functions/',
+  })
+
+  eleventyConfig.addPlugin(EleventyServerlessBundlerPlugin, {
+    name: 'on-demand', // The serverless function name for the permalink object
+    functionsDir: './cloud/functions/',
+  })
+
+  eleventyConfig.addPlugin(eleventyNavigationPlugin)
+
+  eleventyConfig.addGlobalData('ttl', '3600')
+  eleventyConfig.dataFilterSelectors.add('ttl')
+
+  return {
+    dir: {
+      input: 'src',
+      output: 'dist',
+      data: '_data',
+    },
+    templateFormats: ['md', 'njk', 'html'],
+  }
+}
